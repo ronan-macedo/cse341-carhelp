558c31f1dcd38af11f63c5b4593560b5899b3951
Ronan Macedo <rmacedo@enhesa.com>
2024-01-25 00:10:26 +0000

    Feat: Implementing health-check.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 router.use('/', require('./swagger'));
 router.use('/autorescues', require('./autorescues-routes'));
 router.use('/customers', require('./customers-routes'));
+router.use('/health', require('./healthcheck'))
 router.get('/', (req, res) => {    
     res.redirect(process.env.APP_HOST + '/api-docs');
 });
